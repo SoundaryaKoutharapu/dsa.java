ca220110587817cdfde5d3ac97d70cc4af5415c8
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-28 18:29:21 +0530

    bs

--- binarySearch/SearchInRotatedArray.java
+++ binarySearch/SearchInRotatedArray.java
@@ -22,17 +22,17 @@ public class SearchInRotatedArray
       {
         return mid-1;
       }
-      if(arr[start]>=arr[mid])
+      if(arr[mid]<=arr[start])
       {
-         end = mid-1;
+        end = mid-1;
       }
-      if(arr[start]<arr[mid])
+      else
       {
         start = mid+1;
       }
-      }
-    return -1;    
-   }
+    }
+    return -1;
+} 
 
 
     static int pivotBinarySearch(int[] arr, int target)
@@ -88,7 +88,7 @@ public class SearchInRotatedArray
     public static void main(String[] args) 
     {
         int[] arr = {4,5,6,7,8,0,1,2,3};
-        int target = 8;
+        int target = 3;
         int ans = pivotBinarySearch(arr, target);
         System.out.println(ans);
     }
