895b7cfb53acc748b700cdd4667714f208af05dd
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-01 08:44:03 +0530

    modified

--- bubbleSort/BubbleSort.java
+++ bubbleSort/BubbleSort.java
@@ -4,18 +4,19 @@ public class BubbleSort
 {
     public static void main(String[] args) 
     {
-        int[] arr = {12,89,03,67,89,34};
+        int[] arr = {1,2,3,4,5};
           bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void bubble(int[] arr) 
     {
+        boolean swapped;
          for(int i=0;i<arr.length;i++)
          {
+            swapped = false;   
             for(int j=1;j<arr.length-i;j++)
-            {
-
+            { 
                /*  descending order
                if(arr[j-1]<arr[j])
                {
@@ -30,7 +31,15 @@ public class BubbleSort
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
                }
+               swapped = true;
             }
-         }   
+ // if you did not swap for a particular value of i , then it means the program is swapped so you can stop the program     
+        if(!swapped)
+         {
+            break;
+         }
+
+         }  
+         
     }
 }
