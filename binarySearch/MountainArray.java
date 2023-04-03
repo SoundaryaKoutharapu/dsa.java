8d9eb0943d14cf4ca4b8257b3e79fe1cc00d152f
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-03 09:35:09 +0530

    fixed corner cases

--- binarySearch/MountainArray.java
+++ binarySearch/MountainArray.java
@@ -1,4 +1,8 @@
-public class MountainArray
+import javax.lang.model.element.Element;
+
+// https://leetcode.com/problems/find-peak-element/
+
+/* public class MountainArray
 {
    static int searchMountain(int[] arr) 
    {
@@ -23,8 +27,50 @@ public class MountainArray
 
     public static void main(String[] args)
     {
-        int[] arr = {8, 10, 12, 14, 16, 18, 20, 17, 15, 13, 11, 9};
+        int[] arr = {8, 10, 43, 14, 16, 18, 2, 17, 15, 13, 11, 9};
         int ans = searchMountain(arr);
         System.out.println(ans);
     }
 }
+*/
+
+public class MountainArray
+{
+
+
+
+
+    public static void main(String[] args) 
+    {
+        //int[] arr = {16,20,31,4,4,4,0,2,0,1};
+        int[] arr = {1,1,1,1,1,1};
+        System.out.println(bsSearch(arr));
+    }
+
+    static int bsSearch(int[] arr) 
+    {
+        int start = 0;
+        int end  = arr.length-1;
+
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+
+            if(mid==0 || arr[mid]>=arr[mid-1] && mid==arr.length-1 || arr[mid]>=arr[mid+1] )
+            {
+                return mid;
+            }
+            else if(arr[mid]<=arr[mid]-1)
+            {
+                end = mid;
+            }
+            else 
+            {
+                start = mid+1;
+            }
+        }
+
+        return -1;
+    }
+}
+
