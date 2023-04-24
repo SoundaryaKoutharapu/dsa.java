eb3d95b29fe9a38a501425080d44fab801f1ab94
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-24 16:18:35 +0530

    cycle sort

---
+++ cycleSort/FirstMissingPositive.java
@@ -0,0 +1,41 @@
+//   https://leetcode.com/problems/first-missing-positive/
+
+import java.util.Arrays;
+
+public class FirstMissingPositive
+{
+    public static void main(String[] args) 
+    {
+        int [] arr = {1,2,3};
+        int ans = firstmissingSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(ans);   
+    }
+
+  static int firstmissingSort(int[] arr) 
+  {
+    int i = 0;
+    while(i<arr.length)
+    {
+        int correctIndex = arr[i]-1;
+        if(arr[i] >0 && arr[i]<=arr.length && arr[i]!=arr[correctIndex])
+        {
+            int temp = arr[i];
+            arr[i] = arr[correctIndex];
+            arr[correctIndex] = temp;
+        }
+        else
+            i++;
+    }
+
+    for(int j=0; j<arr.length; j++)
+    {
+        if(arr[j]!=j+1)
+        {
+            return j+1;
+        }
+        
+    }
+    return arr.length+1;
+  }
+}
