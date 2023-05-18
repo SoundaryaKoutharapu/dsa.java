bbb1fe954b8ddc083280125b7de73bee25906ad5
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-18 13:44:14 +0530

    updated fibinocci with formula

--- Recursion/FibinocciSeries.java
+++ Recursion/FibinocciSeries.java
@@ -1,5 +1,7 @@
 // fibonocci series using recursion
 
+import java.lang.Math;
+
 public class FibinocciSeries
 {
     static int fibo(int n)
@@ -10,8 +12,16 @@ public class FibinocciSeries
         }
         return fibo(n-1) + fibo(n-2);
     }
+
+    static double fiboFormula(int n)
+    {
+        return Math.pow((1+Math.sqrt(5)/2), n) / Math.sqrt(5);
+    } 
+
+
+
     public static void main(String[] args) 
     {
-      System.out.println( fibo(3));
+      System.out.println( fiboFormula(60));
     }
 }
